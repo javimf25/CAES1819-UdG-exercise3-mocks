25518972c95a584a515e10bfbb42b5de87fa3653
javimf25 <43755480+javimf25@users.noreply.github.com>
2018-11-11 12:23:14 +0100

    Add files via upload

--- src/test/java/org/udg/caes/account/TestAccountService.java
+++ src/test/java/org/udg/caes/account/TestAccountService.java
@@ -15,6 +15,29 @@ public class TestAccountService {
 
   @Test
   void testTransfer(@Tested AccountService as, @Mocked AccountManager am)  {
-    throw new UnsupportedOperationException("Not implemented yet");
+
+    Account a = new Account("Usera",1000);
+    Account b = new Account("Userb",5000);
+
+    new Expectations(){{
+
+      am.findAccount("Usera") ; result=a;
+
+      am.findAccount("Userb"); result=b;
+
+
+    }};
+
+    as.transfer("Usera","Userb",1000);
+
+    assertEquals(0,a.getBalance());
+    assertEquals(6000,b.getBalance());
+
+    new Verifications(){{
+
+      am.updateAccount(a); times=1;
+      am.updateAccount(b); times=1;
+    }};
+    
   }
 }
